604aaca1e80f4388e130e8336f61780b857588ff
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2020-12-23 09:19:46 +0900

    add UCodePoint.ts.

---
+++ app0/smuflfontviewer/lib/UCodePoint.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2020 h-sug1no
+ */
+
+ export class UCodePoint {
+  codepoint_ :number;
+
+  constructor(codepoint: number) {
+    this.codepoint_ = codepoint;
+  }
+
+  public static fromUString(uString: string): UCodePoint {
+    const codepoint = Number.parseInt(uString.replace('U+',''), 16);
+    return new UCodePoint(codepoint);
+  }
+
+  public toUString(addPrefix :boolean = true): string {
+    const str = this.codepoint_.toString(16).toUpperCase();
+    return str.padStart(4, '0');
+  }
+
+  public toCharString(): string {
+    return isNaN(this.codepoint_) ? '?' : String.fromCodePoint(this.codepoint_);
+  }
+
+  public toNumber(): number {
+    return this.codepoint_;
+  }
+};
