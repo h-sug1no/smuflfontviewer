8f2822aaee6c30eb799d7bde6bc62efcdc0d05de
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2020-12-25 14:39:14 +0900

    add _document.tsx to specify roboto CDN.

---
+++ app0/smuflfontviewer/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import React, { ReactElement } from 'react';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <style>{`body { margin: 0 } /* custom!*/`}</style>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
