0ef2137a4c12d5a8d06969491b6c79da7ebdda80
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2021-05-27 08:27:26 +0900

    fix some `any type` warnings.

--- app0/smuflfontviewer/components/GlyphnamesList.tsx
+++ app0/smuflfontviewer/components/GlyphnamesList.tsx
@@ -1,15 +1,16 @@
 /**
  * Copyright (c) 2021 h-sug1no
  */
-
+import { Glyphnames } from '../lib/SMuFLTypes';
 import { Database } from '../lib/SMuFLMetadata';
 import { List } from '@material-ui/core';
 import { createGlyphnameInfo } from '../lib/RenderUtils';
 
-function GlyphnamesList(sMuFLMetadata: Database, glyphnames: any): JSX.Element {
-  const keys = Object.keys(glyphnames);
+function GlyphnamesList(sMuFLMetadata: Database, glyphnames?: Glyphnames): JSX.Element {
+  const dic = glyphnames || {};
+  const keys = Object.keys(dic);
   const ret = keys.map((key) => {
-    return createGlyphnameInfo(sMuFLMetadata, glyphnames[key], key);
+    return createGlyphnameInfo(sMuFLMetadata, dic[key], key);
   });
   return <List>{ret}</List>;
 }
