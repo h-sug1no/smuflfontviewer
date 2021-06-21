3b94cb31faa5e5bad258cc479baccbbe37694dab
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2021-06-21 09:03:24 +0900

    fix warning.

--- app0/smuflfontviewer/pages/_document.tsx
+++ app0/smuflfontviewer/pages/_document.tsx
@@ -1,8 +1,15 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
 import React, { ReactElement } from 'react';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
