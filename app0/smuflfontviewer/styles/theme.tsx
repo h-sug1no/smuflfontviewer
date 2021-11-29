dbf3fffddf8b164bb2747864a646c4c4a07e1807
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2021-11-29 12:48:37 +0900

    [WIP]: remove adaptV4Theme.

--- app0/smuflfontviewer/styles/theme.tsx
+++ app0/smuflfontviewer/styles/theme.tsx
@@ -1,8 +1,8 @@
-import { createTheme, adaptV4Theme } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // A custom theme for this app
-const theme = createTheme(adaptV4Theme({
+const theme = createTheme({
   palette: {
     primary: {
       main: '#556cd6',
@@ -17,6 +17,6 @@ const theme = createTheme(adaptV4Theme({
       default: '#ffe',
     },
   },
-}));
+});
 
 export default theme;
