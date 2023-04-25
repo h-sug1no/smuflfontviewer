aefbaee6cd7e6cbd4dc0aedd475ec1b86c1b2fff
h-sug1no <59550999+h-sug1no@users.noreply.github.com>
2023-04-25 13:46:35 +0900

    support toggled pref state.

--- docs/viewer.js
+++ docs/viewer.js
@@ -452,6 +452,7 @@ class SMuFLFontViewer {
         }
       });
       renderGlyph(currentGlyphData);
+      preferenceElms.onChange();
     });
     const $smuflRenderGlyphOptionsResetGlyphSize = $(
       "#smuflRenderGlyphOptionsResetGlyphSize"
